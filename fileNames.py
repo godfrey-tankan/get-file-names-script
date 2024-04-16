ca6fd79dc5b2ba4fcdcca848f1d58643a040a21b
godfrey-tankan <godfreytantswakandeya@gmail.com>
2024-04-16 12:53:21 +0200

    Refactor fileNames.py to remove unnecessary print statement and improve PDF file listing

--- fileNames.py
+++ fileNames.py
@@ -8,8 +8,6 @@ for file_name in os.listdir(folder_path):
     if file_name.endswith(".pdf"):  
         file_list.append(file_name)
 
-print(file_list )
-
 
 def read_titles(file_list):
     titles = []
